2859efaeb0d20854442279ccf7fb490da442caec
Chosungwoo123 <ejyoo12345@naver.com>
2023-07-30 23:33:57 +0900

    Add a Shot Sound.

--- Source/ShooterGame/ShooterCharacter.h
+++ Source/ShooterGame/ShooterCharacter.h
@@ -65,6 +65,9 @@ private:
 	/** Base look up/down rate, in deg/sec. Other scaling may affect final turn rate */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	float BaseLookUpRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class USoundCue* FireSound;
 	
 public:
 	/** Returns CameraBoom SubObject */
